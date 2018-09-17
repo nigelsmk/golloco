b8e252a0e7b0010f2621e261a93e8f67f2d8f125
nigelsmk <nigelsmk@gmail.com>
2018-09-17 12:57:30 +0800

    test comment out chat id filter

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ let row = 0;
 let linkText;
 
 bot.on('/hello', (msg) => {
-    if (msg.chat.id == -1001290400104) { //-1001321929443
+    //if (msg.chat.id == -1001290400104) { //-1001321929443
         return bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}, I'm Mali and I love to share travel content once a day. Please don't talk to me, Imma recluse!`, { replyToMessage: msg.message_id });
-    }
+    //}
 });
 
 bot.on('/helloMali', (msg) => {
@@ -89,7 +89,7 @@ function getNewToken(oAuth2Client, callback) {
 }
 
 function printTravelArticles(auth) {
-    schedule.scheduleJob('*/5 * * * * *', function () {
+    //schedule.scheduleJob('*/5 * * * * *', function () {
         const sheets = google.sheets({ version: 'v4', auth });
         sheets.spreadsheets.values.get({
             spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
@@ -111,5 +111,5 @@ function printTravelArticles(auth) {
             }
         });
 
-    });
+    //});
 }
